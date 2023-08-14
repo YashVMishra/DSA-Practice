7c3994cf48959870cbe103927b0fd65c50467d34
YashVMishra <yashvardhanmishra01@gmail.com>
2023-08-14 22:14:05 +0530

    New Ques Added

---
+++ DSA-LeetCode Random/Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+import java.util.PriorityQueue;
+
+public class Kth_Largest_Element_in_an_Array {
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 1, 5, 6, 4 };
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (pq.peek() < nums[i]) {
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+}
