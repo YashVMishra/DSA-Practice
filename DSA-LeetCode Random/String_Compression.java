a37456f1086998770fdd520617033e6d60ef4789
YashVMishra <yashvardhanmishra01@gmail.com>
2023-08-23 12:09:15 +0530

    String Compression Ques Updated and Move Zeroes Ques Added

--- DSA-LeetCode Random/String_Compression.java
+++ DSA-LeetCode Random/String_Compression.java
@@ -1,41 +1,38 @@
-//https://leetcode.com/problems/string-compression/description/
+// https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
+
+import java.util.Arrays;
 
 public class String_Compression {
     public static void main(String[] args) {
-        char[] chars = { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' };
+        char[] chars = { 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c' };
         System.out.println(compress(chars));
+        System.out.println(Arrays.toString(chars));
     }
 
     public static int compress(char[] chars) {
+        StringBuilder ans = new StringBuilder();
         int count = 0;
-        String ans = "";
 
         for (int i = 0; i < chars.length; i += count) {
             char ch = chars[i];
             count = 1;
 
-            // second loop to find the count of each character.
             for (int j = i + 1; j < chars.length; j++) {
                 if (chars[j] == ch) {
                     count++;
-                }
+                } else {
 
-                // used break as we want the count of the
-                // consecutive same character.
-                // break usefull when dealing with input as
-                // char[] chars={'a','a', 'b', 'b', 'b','a','a'}
-                // here 'a' occurs two times in a different positions.
-                else {
+                    // break will be useful for the test cases like
+                    // { 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'a', 'a'}
                     break;
                 }
             }
 
             if (count == 1) {
-                ans = ans + ch;
-            }
-
-            else {
-                ans = ans + ch + Integer.toString(count);
+                ans.append(ch);
+            } else {
+                ans.append(ch);
+                ans.append(count);
             }
         }
 
@@ -43,6 +40,6 @@ public class String_Compression {
             chars[i] = ans.charAt(i);
         }
 
-        return count;
+        return ans.length();
     }
 }
