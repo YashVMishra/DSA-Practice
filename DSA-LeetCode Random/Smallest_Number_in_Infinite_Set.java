0f2b9abb3b2c809d1ff912e2dc65ae1d1d6f67d6
YashVMishra <yashvardhanmishra01@gmail.com>
2023-09-28 13:16:24 +0530

    Smallest Number in Infinite Set Ques Updated

--- DSA-LeetCode Random/Smallest_Number_in_Infinite_Set.java
+++ DSA-LeetCode Random/Smallest_Number_in_Infinite_Set.java
@@ -1,12 +1,13 @@
-//https://leetcode.com/problems/smallest-number-in-infinite-set/description/
+// https://leetcode.com/problems/smallest-number-in-infinite-set/description/?envType=study-plan-v2&envId=leetcode-75
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class Smallest_Number_in_Infinite_Set {
     public static void main(String[] args) {
-        SmallestInfiniteSet ob = new SmallestInfiniteSet();
+        SmallestInfiniteSet_2 ob = new SmallestInfiniteSet_2();
         ob.addBack(2);
         System.out.println(ob.popSmallest());
         System.out.println(ob.popSmallest());
@@ -18,6 +19,7 @@ public class Smallest_Number_in_Infinite_Set {
     }
 }
 
+// using ArrayList.
 class SmallestInfiniteSet {
     ArrayList<Integer> list;
     HashSet<Integer> set;
@@ -45,6 +47,36 @@ class SmallestInfiniteSet {
             list.add(num);
             set.add(num);
             Collections.sort(list);
+        }
+    }
+}
+
+// using Heap.
+class SmallestInfiniteSet_2 {
+    PriorityQueue<Integer> pq;
+    HashSet<Integer> set;
+
+    public SmallestInfiniteSet_2() {
+        pq = new PriorityQueue<>();
+        set = new HashSet<>();
+
+        for (int i = 1; i <= 1000; i++) {
+            pq.add(i);
+            set.add(i);
+        }
+    }
+
+    public int popSmallest() {
+        int value = pq.poll();
+        set.remove(value);
+
+        return value;
+    }
+
+    public void addBack(int num) {
+        if (!set.contains(num)) {
+            pq.add(num);
+            set.add(num);
         }
     }
 }
