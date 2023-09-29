a978fffeb651dc3f6036e307842838aa38f665a3
YashVMishra <yashvardhanmishra01@gmail.com>
2023-09-29 13:20:41 +0530

    Maximum Subsequence Score Ques Updated

--- DSA-LeetCode Random/Maximum_Subsequence_Score.java
+++ DSA-LeetCode Random/Maximum_Subsequence_Score.java
@@ -1,48 +1,44 @@
-//https://leetcode.com/problems/maximum-subsequence-score/description/
+// https://leetcode.com/problems/maximum-subsequence-score/description/?envType=study-plan-v2&envId=leetcode-75
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Maximum_Subsequence_Score {
     public static void main(String[] args) {
-        int[] nums1 = { 1, 3, 3, 2 };
-        int[] nums2 = { 2, 1, 3, 4 };
-        int k = 3;
+        int[] nums1 = { 4, 2, 3, 1, 1 };
+        int[] nums2 = { 7, 5, 10, 9, 6 };
+        int k = 1;
 
         System.out.println(maxScore(nums1, nums2, k));
     }
 
     public static long maxScore(int[] nums1, int[] nums2, int k) {
-        int[][] vec = new int[nums1.length][2];
-
+        int[][] temp = new int[nums1.length][2];
         for (int i = 0; i < nums1.length; i++) {
-            vec[i][0] = nums2[i];
-            vec[i][1] = nums1[i];
+            temp[i][0] = nums1[i];
+            temp[i][1] = nums2[i];
         }
 
-        // sort according to the second value in vec and that too is descending order
-        Arrays.sort(vec, (a, b) -> b[0] - a[0]);
-
-        long ans = 0;
-        long curr_sum = 0;
-
-        // pq for deleting the smallest element everytime.
+        // sorting the array acc to nums2 elements in descending order.
+        Arrays.sort(temp, (a, b) -> Integer.compare(b[1], a[1]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        long kSum = 0;
 
-        for (int i = 0; i < k - 1; i++) {
-            curr_sum += vec[i][1];
-            pq.offer(vec[i][1]);
+        // finding the result of first k elements of temp array.
+        for (int i = 0; i < k; i++) {
+            kSum += temp[i][0];
+            pq.add(temp[i][0]);
         }
 
-        for (int i = k - 1; i < nums1.length; i++) {
-            curr_sum += vec[i][1];
-            pq.offer(vec[i][1]);
+        long result = kSum * temp[k - 1][1];
 
-            ans = Math.max(ans, curr_sum * vec[i][0]);
+        for (int i = k; i < temp.length; i++) {
+            kSum += temp[i][0] - pq.poll();
+            pq.add(temp[i][0]);
 
-            curr_sum -= pq.poll();
+            result = Math.max(result, kSum * temp[i][1]);
         }
 
-        return ans;
+        return result;
     }
 }
