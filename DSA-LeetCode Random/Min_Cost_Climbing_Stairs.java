60fdfd9187adafb1ba78d0de73ea0512a875121b
YashVMishra <yashvardhanmishra01@gmail.com>
2023-10-09 01:12:34 +0530

    Min Cost Climbing Stairs Ques Updated and Max Dot Product of Two Subsequences Ques Added

--- DSA-LeetCode Random/Min_Cost_Climbing_Stairs.java
+++ DSA-LeetCode Random/Min_Cost_Climbing_Stairs.java
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/min-cost-climbing-stairs/description/?envType=study-plan&id=level-1
+// https://leetcode.com/problems/min-cost-climbing-stairs/description/?envType=study-plan-v2&envId=leetcode-75
 
 import java.util.Arrays;
 
@@ -9,42 +9,38 @@ public class Min_Cost_Climbing_Stairs {
     }
 
     public static int minCostClimbingStairs(int[] cost) {
-        // return Math.min(helper(cost, 0), helper(cost, 1));
-        int[] dp = new int[cost.length];
-
-        // for this logic see the hint in the given question.
-        // The answer will be the minimum of dp[0] and dp[1].
-        Arrays.fill(dp, -1);
-        helper_2(cost, 0, dp);
-        return Math.min(dp[0], dp[1]);
+        int[] memo = new int[cost.length];
+        Arrays.fill(memo, -1);
+        helper_DP(cost, 0, memo);
+        return Math.min(memo[0], memo[1]);
     }
 
-    // using recursion.
-    public static int helper_1(int[] cost, int i) {
+    // using recursion
+    public static int helper(int[] cost, int i) {
         if (i >= cost.length) {
             return 0;
         }
 
         int ans = 0;
-        ans = Math.min((cost[i] + helper_1(cost, i + 1)), (cost[i] + helper_1(cost, i + 2)));
+        ans = cost[i] + Math.min(helper(cost, i + 1), helper(cost, i + 2));
 
         return ans;
     }
 
     // using DP
-    public static int helper_2(int[] cost, int i, int[] dp) {
-        if (i >= cost.length) {
+    public static int helper_DP(int[] cost, int index, int[] memo) {
+        if (index >= cost.length) {
             return 0;
         }
 
-        if (dp[i] != -1) {
-            return dp[i];
+        if (memo[index] != -1) {
+            return memo[index];
         }
 
         int ans = 0;
-        ans = Math.min((cost[i] + helper_2(cost, i + 1, dp)), (cost[i] + helper_2(cost, i + 2, dp)));
-        dp[i] = ans;
+        ans = cost[index] + Math.min(helper_DP(cost, index + 1, memo), helper_DP(cost, index + 2, memo));
+        memo[index] = ans;
 
-        return dp[i];
+        return ans;
     }
 }
