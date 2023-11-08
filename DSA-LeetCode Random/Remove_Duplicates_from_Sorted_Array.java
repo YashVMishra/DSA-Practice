0ce07a7c87ee7e66485a13e5d4c29531df012ad5
YashVMishra <yashvardhanmishra01@gmail.com>
2023-11-08 19:15:39 +0530

    Remove Duplicates from Sorted Array Ques Added

---
+++ DSA-LeetCode Random/Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class Remove_Duplicates_from_Sorted_Array {
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int index = 0;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                nums[index] = nums[i];
+                index++;
+                set.add(nums[i]);
+            }
+        }
+
+        return index; // you can also return the size of the set.
+    }
+}
