c7e34f6ff4f975dce84880024d7f39daa62fa495
YashVMishra <yashvardhanmishra01@gmail.com>
2023-11-19 19:20:28 +0530

    Reduction Operations to Make the Array Elements Equal Ques Added

---
+++ DSA-LeetCode Random/Reduction_Operations_to_Make_the_Array_Elements_Equal.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Reduction_Operations_to_Make_the_Array_Elements_Equal {
+    public static void main(String[] args) {
+
+    }
+
+    public static int reductionOperations(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int count = 0;
+
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            count += n - i;
+        }
+
+        return count;
+    }
+}
