c63484e7134f59431c1d7cbc3e4f065b3da8628d
YashVMishra <yashvardhanmishra01@gmail.com>
2023-12-26 21:58:59 +0530

    Number of Dice Rolls With Target Sum Ques Added

---
+++ DSA-LeetCode Random/Number_of_Dice_Rolls_With_Target_Sum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Number_of_Dice_Rolls_With_Target_Sum {
+    private static final long M = 1000000007;
+    private static int[][] t = new int[32][1002];
+
+    public static void main(String[] args) {
+        System.out.println(numRollsToTarget(1, 6, 3));
+    }
+
+    public static int numRollsToTarget(int n, int k, int target) {
+        for (int i = 0; i < 32; i++) {
+            Arrays.fill(t[i], -1);
+        }
+
+        return solve(n, k, target);
+    }
+
+    public static int solve(int n, int k, int target) {
+        if (target < 0 || n == 0)
+            return target == 0 ? 1 : 0;
+
+        if (t[n][target] != -1)
+            return t[n][target];
+
+        int sum = 0;
+
+        for (int i = 1; i <= k; i++) {
+            sum = (sum + solve(n - 1, k, target - i)) % (int) M;
+        }
+
+        return t[n][target] = sum;
+    }
+}
