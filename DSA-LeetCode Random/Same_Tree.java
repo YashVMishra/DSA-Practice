770b3d65ae2cc850b8a7895f920e4ad660a991b6
YashVMishra <yashvardhanmishra01@gmail.com>
2024-02-26 17:54:45 +0530

    Same Tree Ques Added

---
+++ DSA-LeetCode Random/Same_Tree.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/same-tree/description/?envType=daily-question&envId=2024-02-26
+
+public class Same_Tree {
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        System.out.println(isSameTree(p, q));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
