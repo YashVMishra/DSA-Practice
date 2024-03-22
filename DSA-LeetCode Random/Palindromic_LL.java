6cc04c846e6007dc2c68f56d30194c47fcbcaa7a
YashVMishra <yashvardhanmishra01@gmail.com>
2024-03-22 19:01:41 +0530

    Palindrome Linked List Ques Added

--- DSA-LeetCode Random/Palindromic_LL.java
+++ DSA-LeetCode Random/Palindromic_LL.java
@@ -59,4 +59,51 @@ public class Palindromic_LL {
         return true;
 
     }
+
+    // -------------------OR-------------------
+
+    public static boolean isPalindrome_2(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode revhead = reverseList(slow);
+        prev.next = null;
+
+        while (revhead != null && head != null) {
+            if (revhead.val != head.val) {
+                return false;
+            }
+
+            revhead = revhead.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
 }
