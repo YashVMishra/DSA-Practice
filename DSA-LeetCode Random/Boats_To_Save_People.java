6aaa4982818b130558fa000a6470cabdb7dfcbb5
YashVMishra <yashvardhanmishra01@gmail.com>
2024-05-04 11:43:04 +0530

    Boats to Save People Ques Updated

--- DSA-LeetCode Random/Boats_To_Save_People.java
+++ DSA-LeetCode Random/Boats_To_Save_People.java
@@ -12,24 +12,25 @@ public class Boats_To_Save_People {
     public static int numRescueBoats(int[] people, int limit) {
         int start = 0;
         int end = people.length - 1;
-        int count = 0;
+        int boats = 0;
         Arrays.sort(people);
 
         while (start <= end) {
+
+            // try to send the heaviest person with the lightest person
+            // if possible then send them else send the heaviest person alone
             int sum = people[start] + people[end];
+
             if (sum <= limit) {
                 start++;
                 end--;
-            }
-
-            else {
+            } else {
                 end--;
             }
 
-            count++;
+            boats++;
         }
 
-        return count;
-
+        return boats;
     }
 }
