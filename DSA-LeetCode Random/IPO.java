f61e02460ae6d54d7a1fb2063d768e96dfe96481
YashVMishra <yashvardhanmishra01@gmail.com>
2024-06-15 20:08:17 +0530

    IPO Ques Updated

--- DSA-LeetCode Random/IPO.java
+++ DSA-LeetCode Random/IPO.java
@@ -1,5 +1,9 @@
 //https://leetcode.com/problems/ipo/description/
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class IPO {
@@ -39,6 +43,36 @@ public class IPO {
                     break;
                 }
             }
+        }
+
+        return w;
+    }
+
+    // ---------------------------------------------------------------------------------------
+
+    public static int findMaximizedCapital_2(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        List<int[]> projects = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            projects.add(new int[] { capital[i], profits[i] });
+        }
+
+        projects.sort(Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> maxProfitHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int i = 0;
+
+        while (k-- > 0) {
+            while (i < n && projects.get(i)[0] <= w) {
+                maxProfitHeap.add(projects.get(i)[1]);
+                i++;
+            }
+
+            if (maxProfitHeap.isEmpty()) {
+                break;
+            }
+
+            w += maxProfitHeap.poll();
         }
 
         return w;
